33d26a19f5c61dcc9e75f11ee73bc6f0135e2c8b
Pothala Sri Chaitanya <pothalasrichaitanya@Pothalas-MacBook-Pro.local>
2025-07-16 23:02:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='dept'>
+    Itachi is learning cse <br/>
+    <label className='dept'>information is everthing and valeable </label>
+   </div>
+  )
+}
+
+export default App
